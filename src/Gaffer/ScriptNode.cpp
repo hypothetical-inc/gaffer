7098fcbaa10cd65aca73f5cb74ce72f2d8599ec6
Eric Mehl <eric@thinkhypothetical.com>
2018-07-26 13:50:43 -0400

    Revert "change readonly testing to be cross-platform"
    This reverts commit 23a2ae5a7e6ff8224be66ec96abf7a4f10b2db01.

--- src/Gaffer/ScriptNode.cpp
+++ src/Gaffer/ScriptNode.cpp
@@ -59,6 +59,8 @@
 
 #include <fstream>
 
+#include <unistd.h>
+
 using namespace Gaffer;
 
 //////////////////////////////////////////////////////////////////////////
@@ -831,9 +833,10 @@ void ScriptNode::plugSet( Plug *plug )
 			testOpen.close();
 		}
 
+		context()->set( g_scriptName, fileName.stem().string() );
 		MetadataAlgo::setReadOnly(
 			this,
-			isReadOnly,
+			boost::filesystem::exists( fileName ) && 0 != access( fileName.c_str(), W_OK ),
 			/* persistent = */ false
 		);
 	}
