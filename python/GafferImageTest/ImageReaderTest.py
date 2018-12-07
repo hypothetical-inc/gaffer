d0f1a84494bbf9a51a09b8dbbc28671f17d750ce
Eric Mehl <eric@thinkhypothetical.com>
2018-12-07 17:52:07 -0500

    ImageReaderTest : adapt for Windows

--- python/GafferImageTest/ImageReaderTest.py
+++ python/GafferImageTest/ImageReaderTest.py
@@ -55,9 +55,9 @@ class ImageReaderTest( GafferImageTest.ImageTestCase ) :
 			gaffer_root = os.path.expandvars( "$GAFFER_ROOT" )
 
 		self.fileName = os.path.abspath( gaffer_root + "/python/GafferImageTest/images/circles.exr" )
-		self.colorSpaceFileName = os.path.abspath( gaffer_root + "$GAFFER_ROOT/python/GafferImageTest/images/circles_as_cineon.exr" )
-		self.offsetDataWindowFileName = os.path.abspath( gaffer_root + "$GAFFER_ROOT/python/GafferImageTest/images/rgb.100x100.exr" )
-		self.jpgFileName = os.path.abspath( gaffer_root + "$GAFFER_ROOT/python/GafferImageTest/images/circles.jpg" )
+		self.colorSpaceFileName = os.path.abspath( gaffer_root + "/python/GafferImageTest/images/circles_as_cineon.exr" )
+		self.offsetDataWindowFileName = os.path.abspath( gaffer_root + "/python/GafferImageTest/images/rgb.100x100.exr" )
+		self.jpgFileName = os.path.abspath( gaffer_root + "/python/GafferImageTest/images/circles.jpg" )
 
 		GafferImageTest.ImageTestCase.setUp( self )
 		self.__defaultColorSpaceFunction = GafferImage.ImageReader.getDefaultColorSpaceFunction()
