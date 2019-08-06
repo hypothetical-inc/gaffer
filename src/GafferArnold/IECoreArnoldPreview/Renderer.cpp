f439b4f3d45863684601fa4272f8badb4422c4ea
Eric Mehl <eric@thinkhypothetical.com>
2019-08-06 13:28:58 -0400

    IECoreArnoldPreview : MSVC can't compare IECore::InternedString to std::string directly

--- src/GafferArnold/IECoreArnoldPreview/Renderer.cpp
+++ src/GafferArnold/IECoreArnoldPreview/Renderer.cpp
@@ -894,7 +894,7 @@ class ArnoldAttributes : public IECoreScenePreview::Renderer::AttributesInterfac
 					}
 				}
 
-				if( it->first.string() == g_arnoldLightFilterShaderAttributeName )
+				if( it->first.string() == g_arnoldLightFilterShaderAttributeName.string() )
 				{
 					continue;
 				}
