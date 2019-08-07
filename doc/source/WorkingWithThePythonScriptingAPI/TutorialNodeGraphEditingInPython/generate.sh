7d3a6f141aa3500792f8f3a264b235bb2816f254
Eric Mehl <eric@thinkhypothetical.com>
2019-08-07 17:44:46 -0400

    Documentation : convert from shell scripts to Python scripts for Windows compatibility

--- doc/source/WorkingWithThePythonScriptingAPI/TutorialNodeGraphEditingInPython/generate.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# BuildTarget: images/nodes.png
-
-set -e
-
-cp $GAFFER_ROOT/graphics/nodes.png images
-cp $GAFFER_ROOT/graphics/plug.png images
-cp $GAFFER_ROOT/graphics/values.png images
