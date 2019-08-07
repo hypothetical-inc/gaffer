9616023c0d78bce268961903bea7bec0ad32169d
Eric Mehl <eric@thinkhypothetical.com>
2019-08-07 17:44:46 -0400

    Documentation : convert from shell scripts to Python scripts for Windows compatibility

---
+++ doc/source/WorkingWithScenes/Camera/generate.py
@@ -0,0 +1,16 @@
+# BuildTarget: images/gafferSceneUIRotateTool.png
+# BuildTarget: images/gafferSceneUITranslateTool.png
+# BuildTarget: images/cameraOff.png
+# BuildTarget: images/gafferSceneUICameraTool.png
+# BuildTarget: images/toggleOff.png
+# BuildTarget: images/plus.png
+
+import shutil
+import os
+
+shutil.copyfile( os.path.join( os.environ["$GAFFER_ROOT"], "graphics", "gafferSceneUIRotateTool.png" ), os.path.join( "images", "gafferSceneUIRotateTool.png" ) )
+shutil.copyfile( os.path.join( os.environ["$GAFFER_ROOT"], "graphics", "gafferSceneUITranslateTool.png" ), os.path.join( "images", "gafferSceneUITranslateTool.png" ) )
+shutil.copyfile( os.path.join( os.environ["$GAFFER_ROOT"], "graphics", "cameraOff.png" ), os.path.join( "images", "cameraOff.png" ) )
+shutil.copyfile( os.path.join( os.environ["$GAFFER_ROOT"], "graphics", "gafferSceneUICameraTool.png" ), os.path.join( "images", "gafferSceneUICameraTool.png" ) )
+shutil.copyfile( os.path.join( os.environ["$GAFFER_ROOT"], "graphics", "toggleOff.png" ), os.path.join( "images", "toggleOff.png" ) )
+shutil.copyfile( os.path.join( os.environ["$GAFFER_ROOT"], "graphics", "plus.png" ), os.path.join( "images", "plus.png" ) )
