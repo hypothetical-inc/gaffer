b8d7c5a64685c0b0799c876ae41bb851f39a2f59
Eric Mehl <eric@thinkhypothetical.com>
2019-11-08 09:47:48 -0500

    NameSwitch : MSVC needs std::numeric_limits

--- src/Gaffer/NameSwitch.cpp
+++ src/Gaffer/NameSwitch.cpp
@@ -79,7 +79,7 @@ void NameSwitch::setup( const Plug *plug )
 		Plug::In,
 		element,
 		2,
-		Imath::limits<size_t>::max(),
+		std::numeric_limits<size_t>::max(),
 		Plug::Default,
 		/* resizeWhenInputsChange = */ false
 	);
